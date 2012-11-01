eab9f6ca56eece68d6d2271cb2a0a4736f2c4e03
ArturT <arturtrzop@gmail.com>
2012-11-01 13:16:07 +0100

    Rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrakDroid::Application.config.secret_token = '630a1919a886db2aae3f0aa8527e18e643b3370a45325256ce3c0336e5a5931f58e49a94a0e228a1d89a2cb0be439eafeae82e040f85a0dbfb3ee69393a5b992'
