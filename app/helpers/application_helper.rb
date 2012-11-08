0cc1315eb4c9b7e8922accc2ab70748e7dfdc586
ArturT <arturtrzop@gmail.com>
2012-11-08 23:45:09 +0100

    Add choose language links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def banner(img, url, title)
     link_to image_tag(image_path(img)), url, title: title, class: 'banner'
   end
+
+  def choose_language
+    lang = params[:locale]
+
+    pl = link_to 'PL', '/'
+    en = link_to 'EN', '/en'
+
+    if lang == 'en'
+      raw "#{pl} <u>EN</u>"
+    else
+      raw "<u>PL</u> #{en}"
+    end
+  end
 end
