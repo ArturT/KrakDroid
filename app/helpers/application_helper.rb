90adcdc5f8c2f4c7d6bc3959bc6b62fc6842bb96
ArturT <arturtrzop@gmail.com>
2012-11-16 00:28:10 +0100

    Set positions for menu tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,18 @@ module ApplicationHelper
   def speaker_photo(photo_url)
     raw %{<div class="circle photo" style="background-image: url(#{asset_path(photo_url)});"><div class="circle gray_layer"></div></div>}
   end
+  
+  def menu_arrow(title, jump_to, left_or_right)
+    title_text = t "logo.menu.#{title}"
+    raw %{
+    <div class="menu_arrow_container menu_end_#{left_or_right}" id="menu_arrow_#{title}" onclick="go_to_by_scroll('#{jump_to}',0,22)">
+      <div class="menu_bg_#{left_or_right}">
+        <div class="arrow_gradient_dimensions arrow_gradient_#{left_or_right} text">
+          #{title_text}
+        </div>
+      </div>
+      <div class="menu_arrow_dimensions menu_arrow_#{left_or_right}"></div>
+    </div>
+    }
+  end
 end
