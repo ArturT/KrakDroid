445e6c2378e827e16eabe3648915adde4b6fb1e2
ArturT <arturtrzop@gmail.com>
2012-11-18 14:28:07 +0100

    Add scope in admin speakers

--- app/admin/speakers.rb
+++ app/admin/speakers.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Speaker do
-  
+  scope :all do
+    Speaker
+  end
+
+  scope :speakers do
+    Speaker.speakers
+  end
+
+  scope :organizers do
+    Speaker.organizers
+  end
 end
