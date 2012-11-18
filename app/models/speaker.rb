238ca45caf35b399a1c572489304fff8933db348
ArturT <arturtrzop@gmail.com>
2012-11-18 13:51:01 +0100

    Add validation to models

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,7 +1,14 @@
 class Speaker < ActiveRecord::Base
-  attr_accessible :description_en, :description_pl, :name, :organizer
+  attr_accessible :name, :description_pl, :description_en, :organizer
 
-  has_many :schedules
+  has_many :schedules, dependent: :destroy
+
+  scope :speakers, where(organizer: false).order('name ASC')
+  scope :organizers, where(organizer: true).order('name ASC')
+
+  validates :name, presence: true
+  validates :description_pl, presence: true
+  validates :description_en, presence: true
 
   def description
     if I18n.locale == :en
