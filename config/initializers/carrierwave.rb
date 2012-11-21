19ae3768ed091a721f7be3d09ddb1c3622ddeaad
ArturT <arturtrzop@gmail.com>
2012-11-21 03:36:00 +0100

    Set ftp passive to true

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,4 +5,5 @@ CarrierWave.configure do |config|
   config.ftp_passwd = ENV['FTP_PASSWD'] || Settings.ftp.passwd
   config.ftp_folder = ENV['FTP_FOLDER'] || Settings.ftp.folder
   config.ftp_url = ENV['FTP_URL'] || Settings.ftp.url
+  config.ftp_passive = true # Heroku does not support active mode
 end
