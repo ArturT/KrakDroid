93a76df65263d590e336cd44e0d8b14f44bbd7a7
ArturT <arturtrzop@gmail.com>
2012-12-04 23:20:27 +0100

    A few fixes

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ActiveRecord::Base
   attr_accessible :name_en, :name_pl
 
-  has_many :schedules
+  has_many :schedules, dependent: :destroy
 
   validates :name_pl, presence: true
   validates :name_en, presence: true
