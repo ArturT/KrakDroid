2d9ce866e84b65a0f4e97f121885ace960efd0d7
ArturT <arturtrzop@gmail.com>
2013-11-23 14:11:19 +0100

    Add default url for avatar

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -28,6 +28,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url
+    "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=blank&f=y&s=200"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
